398ff9bf93bf9bae1080535b469f5313d6a49bbc
Chris Platts <chris@virelai.co.uk>
2017-06-19 16:48:03 +0100

    Bringing it up to date

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -40,7 +40,7 @@ const reload = browserSync.reload;
 
 // Lint JavaScript
 gulp.task('lint', () =>
-  gulp.src(['app/scripts/**/*.js','!node_modules/**'])
+  gulp.src(['app/scripts/**/*.js', '!app/scripts/library/**', '!node_modules/**'])
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
@@ -62,7 +62,7 @@ gulp.task('nunjucks', function() {
   .pipe(nunjucksRender({
       path: ['app/templates']
     }))
-  .pipe(gulp.dest('.tmp/html'))
+  .pipe(gulp.dest('.tmp'))
 });
 
 // Copy all files at the root level (app)
@@ -106,7 +106,7 @@ gulp.task('styles', () => {
     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
     .pipe(gulp.dest('.tmp/styles'))
     // Concatenate and minify styles
-    .pipe($.if('*.css', $.cssnano()))
+    // .pipe($.if('*.css', $.cssnano()))
     .pipe($.size({title: 'styles'}))
     .pipe($.sourcemaps.write('./'))
     .pipe(gulp.dest('dist/styles'))
@@ -140,7 +140,11 @@ gulp.task('scripts', () =>
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
-  return gulp.src(['app/**/*.html','.tmp/html/**/*.html'])
+  return gulp.src([
+    'app/**/*.html',
+    '!app/styles/**/*.html',
+    '.tmp/**/*.html'
+  ])
     .pipe($.useref({
       searchPath: '{.tmp, app}',
       noAssets: true
@@ -167,7 +171,7 @@ gulp.task('html', () => {
 gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
 // Watch files for changes & reload
-gulp.task('serve', ['scripts', 'styles'], () => {
+gulp.task('serve', ['nunjucks', 'scripts', 'styles'], () => {
   browserSync({
     notify: false,
     // Customize the Browsersync console logging prefix
@@ -180,9 +184,10 @@ gulp.task('serve', ['scripts', 'styles'], () => {
     // https: true,
     server: ['.tmp', 'app'],
     port: 3000
-  });
+  })
 
   gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/**/*.njk'], ['nunjucks', reload]);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
   gulp.watch(['app/images/**/*'], reload);
